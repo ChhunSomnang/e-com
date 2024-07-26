a9e221bc5e1c38523a1a968b2bb7ba87d8d4a0a3
ChhunSomnang <somnangchhun07@gmail.com>
2024-07-26 17:40:55 +0700

    first commit

---
+++ components/Slider.tsx
@@ -0,0 +1,101 @@
+"use client";
+import Image from "next/image";
+import Link from "next/link";
+import React, { useEffect, useState } from "react";
+
+const slides = [
+  {
+    id: 1,
+    title: "Street Outfits",
+    description: "Sale! Up to 50% off!",
+    img: "https://i.pinimg.com/736x/fb/d0/99/fbd099d17624fe0450f7d19b64201bd2.jpg",
+    url: "/",
+    bg: "bg-gradient-to-r from-yellow-50 to-pink-50",
+  },
+  {
+    id: 2,
+    title: "Street Outfits",
+    description: "Sale! Up to 50% off!",
+    img: "https://i.pinimg.com/564x/6a/be/99/6abe991f68daeb8f857787b06cc41fe5.jpg",
+    url: "/",
+    bg: "bg-gradient-to-r from-yellow-50 to-pink-50",
+  },
+  {
+    id: 3,
+    title: "Street Outfits",
+    description: "Sale! Up to 50% off!",
+    img: "https://i.pinimg.com/564x/ba/8c/76/ba8c76f340b2585bdaf3a984e85b81fb.jpg",
+    url: "/",
+    bg: "bg-gradient-to-r from-yellow-50 to-pink-50",
+  },
+];
+
+const Slider = () => {
+  const [current, setCurrent] = useState(0);
+
+  useEffect(()=>{
+    const interval = setInterval(()=>{
+        setCurrent((prev)=>(prev === slides.length -1 ? 0 : prev+1));
+    },3000);
+
+    return()=> clearInterval(interval);
+  },[]);
+
+  return (
+    <div className="h-[calc(100vh-80px)] overflow-hidden ">
+      <div
+        className="w-max h-full flex transition-all ease-in-out duration-1000"
+        style={{ transform: `translateX(-${current * 100}vw)` }}
+      >
+        {slides.map((slide) => (
+          <div
+            className={`${slide.bg} w-screen h-full flex flex-col gap-16 xl:flex-row`}
+            key={slide.id}
+          >
+            {/*Text*/}
+            <div className="h-1/2 xl:w-1/2 xl:h-full flex flex-col items-center justify-center gap-8 2xl:gap-12 text-center">
+              <h2 className="text-xl lg:text-3xl 2xl:text-5xl">
+                {slide.description}
+              </h2>
+              <h1 className="text-5xl lg:text-6xl 2xl:text-8xl font-semibold">
+                {slide.title}
+              </h1>
+              <Link href={slide.url}>
+                <button className="rounded-md bg-black text-white py-3 px-4">
+                  SHOP NOW!
+                </button>
+              </Link>
+            </div>
+            {/*Image*/}
+            <div className="h-1/2 xl:w-1/2 xl:h-full relative">
+              <Image
+                src={slide.img}
+                alt=""
+                fill
+                sizes="100%"
+                className="object-cover"
+              />
+            </div>
+          </div>
+        ))}
+      </div>
+      <div className="absolute m-auto left-1/2 bottom-8 flex gap-4 ">
+        {slides.map((slide, index) => (
+          <div
+            className={`w-3 h-3 rounded-full ring-1 ring-gray-600 cursor-pointer flex items-center justify-center ${
+              current === index ? "scale-150" : ""
+            }`}
+            key={slide.id}
+            onClick={() => setCurrent(index)}
+          >
+            {current === index && (
+              <div className="w-[6px] h-[6px] bg-gray-600 rounded-full"></div>
+            )}
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Slider;
